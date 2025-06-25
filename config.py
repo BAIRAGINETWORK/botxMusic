4036274736543db6620723bd598a972f1d217380
BAIRAGINETWORK <nobitavc95@gmail.com>
2025-06-25 22:07:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,9 +29,9 @@ OWNER_ID = int(getenv("OWNER_ID", None))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
-HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
+HEROKU_APP_NAME = getenv("akaia")
 # Get it from http://dashboard.heroku.com/account
-HEROKU_API_KEY = getenv("HEROKU_API_KEY")
+HEROKU_API_KEY = getenv("HRKU-AASkbm3lArCcxEvBxzTmJqtVxUlXq9Q-mZVy3XR-XWXA_wGDmMvhY2Cm")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
